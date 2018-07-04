0d03da5187f2afc5d7e0402b4884ed94105b8a82
YerikZ <yerik.yilinzhu@gmail.com>
2018-07-04 11:42:05 +0100

    Update DB_Connection.py

--- Database/DB_Connection.py
+++ Database/DB_Connection.py
@@ -7,3 +7,7 @@ from sqlalchemy import types
 engine = create_engine('postgresql://postgres:{password}@localhost:5432/postgres')
 
 multi = pd.read_sql_query("select * from schema.table_name", con=engine)
+
+multi.to_sql(name='tbl_multi', con=engine, schema='**', index=False, if_exists='replace',
+           dtype={'style_code': types.String, 
+                      'story': types.String})
